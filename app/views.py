d4a7f61727bfda277f65db5fc5218fe454f3c221
BernalFA <fabernal1230@gmail.com>
2025-07-22 10:43:59 +0200

    add new fields to start site

--- app/views.py
+++ app/views.py
@@ -20,7 +20,9 @@ def index():
     user_form = UserDataForm()
     if user_form.validate_on_submit():
         entry = UserManager(username=user_form.username.data,
-                            membership=user_form.membership.data)
+                            membership=user_form.membership.data,
+                            delivery=user_form.delivery.data,
+                            include_structures=user_form.structures.data == "true")
         try:
             db.session.add(entry)
             db.session.commit()
